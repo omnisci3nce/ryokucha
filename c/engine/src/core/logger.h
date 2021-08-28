3f00a7373dd15570bf66865fdf4f0bc17176db83
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2021-08-28 12:30:20 +1000

    Use RC as prefix

--- c/engine/src/core/logger.h
+++ c/engine/src/core/logger.h
@@ -1,52 +1,60 @@
-// #pragma once
+#pragma once
 
-// #include "defines.h"
-// #include <stdbool.h>
+#include "defines.h"
+#include <stdbool.h>
 
-// #define LOG_WARN_ENABLED 1
-// #define LOG_INFO_ENABLED 1
-// #define LOG_DEBUG_ENABLED 1
-// #define LOG_TRACE_ENABLED 1
+#define LOG_WARN_ENABLED 1
+#define LOG_INFO_ENABLED 1
+#define LOG_DEBUG_ENABLED 1
+#define LOG_TRACE_ENABLED 1
+// FATAL and ERROR are always logged so don't get a switch
 
-// #if RCRELEASE == 1
-// #define LOG_DEBUG_ENABLED 0
-// #define LOG_TRACE_ENABLED 0
-// #endif
+#if RCRELEASE == 1
+#define LOG_DEBUG_ENABLED 0
+#define LOG_TRACE_ENABLED 0
+#endif
 
-// typedef enum log_level {
-//     LOG_LEVEL_FATAL = 0,
-//     LOG_LEVEL_ERROR,
-//     LOG_LEVEL_WARN,
-//     LOG_LEVEL_INFO,
-//     LOG_LEVEL_DEBUG,
-//     LOG_LEVEL_TRACE
-// } log_level;
+typedef enum log_level {
+    LOG_LEVEL_FATAL = 0,
+    LOG_LEVEL_ERROR,
+    LOG_LEVEL_WARN,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_TRACE
+} log_level;
 
-// bool initialise_logging();
-// void shutdown_logging();
+bool initialise_logging();
+void shutdown_logging();
 
-// RAPI void log_output(log_level level, const char* message, ...);
+// Export this
+RCAPI void log_output(log_level level, const char* message, ...); // uses variadic arguments
 
-// #if LOG_WARN_ENABLED == 1
-// #define RWARN(message, ...) log_output(LOG_LEVEL_WARN, message, ##__VA_ARGS__);
-// #else
-// #define RWARN(message, ...)'
-// #endif
+#define RCFATAL(message, ...) log_output(LOG_LEVEL_FATAL, message, ##__VA_ARGS__);
 
-// #if LOG_INFO_ENABLED == 1
-// #define RINFO(message, ...) log_output(LOG_LEVEL_INFO, message, ##__VA_ARGS__);
-// #else
-// #define RINFO(message, ...)'
-// #endif
+#ifndef RCERROR
+#define RCERROR(message, ...) log_output(LOG_LEVEL_ERROR, message, ##__VA_ARGS__);
+#endif
 
-// #if LOG_DEBUG_ENABLED == 1
-// #define RDEBUG(message, ...) log_output(LOG_LEVEL_DEBUG, message, ##__VA_ARGS__);
-// #else
-// #define RDEBUG(message, ...)'
-// #endif
+#if LOG_WARN_ENABLED == 1
+#define RCWARN(message, ...) log_output(LOG_LEVEL_WARN, message, ##__VA_ARGS__);
+#else
+#define RCWARN(message, ...)'
+#endif
 
-// #if LOG_INFO_ENABLED == 1
-// #define RTRACE(message, ...) log_output(LOG_LEVEL_TRACE, message, ##__VA_ARGS__);
-// #else
-// #define RTRACE(message, ...)'
-// #endif
+#if LOG_INFO_ENABLED == 1
+#define RCINFO(message, ...) log_output(LOG_LEVEL_INFO, message, ##__VA_ARGS__);
+#else
+#define RCINFO(message, ...)'
+#endif
+
+#if LOG_DEBUG_ENABLED == 1
+#define RCDEBUG(message, ...) log_output(LOG_LEVEL_DEBUG, message, ##__VA_ARGS__);
+#else
+#define RCDEBUG(message, ...)'
+#endif
+
+#if LOG_TRACE_ENABLED == 1
+#define RCTRACE(message, ...) log_output(LOG_LEVEL_TRACE, message, ##__VA_ARGS__);
+#else
+#define RCTRACE(message, ...)'
+#endif
